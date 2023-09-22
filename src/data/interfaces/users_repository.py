546242e0699d3a6d1724d3696358658593500812
Bruno Correia <bruninhocorreia2013@gmail.com>
2023-09-22 10:51:43 -0300

    feat: implementacao dos testes e userRepoSpy

--- src/data/interfaces/users_repository.py
+++ src/data/interfaces/users_repository.py
@@ -7,7 +7,6 @@ class UsersRepositoryInterface(ABC):
     @abstractmethod
     def insert_user(self, first_name: str, last_name: str, age: int) -> None: pass
         
-
     @abstractmethod
     def select_user(self, first_name: str) -> List[Users]: pass
         
--- src/data/use_cases/user_finder.py
+++ src/data/use_cases/user_finder.py
@@ -1,3 +1,4 @@
+#pylint: disable=broad-exception-raised
 from typing import Dict
 from src.domain.use_cases.user_finder import UserFinder as UserFinderInterface
 from src.data.interfaces.users_repository import UsersRepositoryInterface
@@ -7,4 +8,20 @@ class UserFinder(UserFinderInterface):
         self.__users_repository = users_repository
 
     def find(self, first_name: str) -> Dict:
-        pass
+        if not first_name.isalpha():
+            raise Exception('Nome inválido para a busca')
+        
+        if len(first_name) > 18:
+            raise Exception('Nome muito grande para a busca')
+        
+        users = self.__users_repository.select_user(first_name)
+        if users == []:
+            raise Exception('Usuário não encontrado')
+        
+        response = {
+            "type": "Users",
+            "count": len(users),
+            "attributes": users
+        }
+ 
+        return response
