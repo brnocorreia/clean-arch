293a7b0cc7e185f79452f235a64e9548e45b9285
Bruno Correia <bruninhocorreia2013@gmail.com>
2023-09-22 13:07:55 -0300

    feat: implementação completa do user_finder e seus testes

--- src/data/use_cases/user_finder.py
+++ src/data/use_cases/user_finder.py
@@ -1,5 +1,6 @@
 #pylint: disable=broad-exception-raised
-from typing import Dict
+from typing import Dict, List
+from src.domain.models.users import Users
 from src.domain.use_cases.user_finder import UserFinder as UserFinderInterface
 from src.data.interfaces.users_repository import UsersRepositoryInterface
 
@@ -8,20 +9,34 @@ class UserFinder(UserFinderInterface):
         self.__users_repository = users_repository
 
     def find(self, first_name: str) -> Dict:
+        self.__validate_name(first_name)
+        users = self.__search_user(first_name)
+        response = self.__format_response(users)
+
+        return response
+    
+    @classmethod
+    def __validate_name(cls, first_name: str) -> None:
         if not first_name.isalpha():
             raise Exception('Nome inválido para a busca')
         
         if len(first_name) > 18:
             raise Exception('Nome muito grande para a busca')
-        
+
+    def __search_user(self, first_name: str) -> List[Users]:
         users = self.__users_repository.select_user(first_name)
-        if users == []:
-            raise Exception('Usuário não encontrado')
-        
+        if users == []: raise Exception('Usuário não encontrado')
+        return users
+
+    @classmethod
+    def __format_response(cls, users: List[Users]) -> Dict:
+        attributes = []
+        for user in users:
+            attributes.append({"first_name": user.first_name,"last_name": user.last_name, "age": user.age})
+
         response = {
             "type": "Users",
             "count": len(users),
-            "attributes": users
+            "attributes": attributes
         }
- 
         return response
--- src/data/use_cases/user_finder_test.py
+++ src/data/use_cases/user_finder_test.py
@@ -14,3 +14,43 @@ def test_find():
     assert response['type'] == 'Users'
     assert response['count'] == len(response['attributes'])
     assert response['attributes'] != ['']
+
+def test_find_error_invalid_name():
+    first_name = 'meunome123'
+
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as exception:
+        assert str(exception) == 'Nome inválido para a busca'
+
+def test_find_error_longer_name():
+    first_name = 'meunomedadadasdasadadadadadasaasasadada'
+
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as exception:
+        assert str(exception) == 'Nome muito grande para a busca'
+
+def test_find_error_user_not_found():
+    class UsersRepositoryError(UsersRepositorySpy):
+        def select_user(self, first_name: str):
+            return []
+    
+    first_name = 'meuNome'
+
+    repo = UsersRepositoryError()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as exception:
+        assert str(exception) == 'Usuário não encontrado' 
